79bb52d1c0a0f745cd6dca77cf29c4b618011b19
daiy <daiy@investoday.com.cn>
2019-08-10 11:41:10 +0800

    init repo

---
+++ src/main/java/com/maurice/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.maurice.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: Maurice
+ * @Date: 2019/8/10
+ * @Description:
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        return "hello";
+    }
+}
